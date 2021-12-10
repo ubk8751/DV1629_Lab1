78aaf5740c168cd2f1916b3984fd35db946a53fd
Emil Karlström <emil.karlstrom@gmail.com>
2021-12-10 15:44:28 +0100

    Comments

--- assignment_fork.c
+++ assignment_fork.c
@@ -9,35 +9,33 @@ int main(int argc, char **argv)
     unsigned i;
     unsigned niterations = 1000;
 
-    // fork twice and save the child IDs
+    // Fork 1
     pid1 = fork();
 
-    //If pid1 > 0 then we're in the parent
+    //If pid1 > 0, then we're in the parent
     if (pid1 > 0) {
+        // Fork 2
         pid2 = fork();
         
-        // If pid2 > 0 then we're in the parent
+        // If pid2 > 0, then we're in the parent
         if(pid2 > 0){
           printf("Parent: Child processes: %d, %d\n", pid1, pid2);
           for (i = 0; i < niterations; ++i)
               printf("B = %d, ", i);
         } 
+        // If pid2 == 0, then we're in child 2
         else if(pid2 == 0) {
           printf("Child 2\n");
           for (i = 0; i < niterations; ++i)
               printf("C = %d, ", i);
-        } else {
-          printf("Unknown process\n");
         }
      }
 
-     //If pid1 == 0, it's thread 1
+     //If pid1 == 0, then we're in child 1
      else if (pid1 == 0){
         printf("Child 1\n");
         for (i = 0; i < niterations; ++i)
             printf("A = %d, ", i);
-     } else {
-       printf("Unknown process\n");
      }
     printf("\n");
 }
